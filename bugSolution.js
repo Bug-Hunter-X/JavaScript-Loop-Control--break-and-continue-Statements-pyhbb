9bc7b646e7ba3f8d2ea9d7e0b42e16fdc2835d3e
BugHunter-X <askzenai@gmail.com>
2025-01-30 23:26:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i === 5) {
+      //This is correct and you should continue your code without any issues
+    }
+  }
+}
